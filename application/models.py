1accf2c9017aea52fb8d2559173154fdfad7722b
Hemanth V. Alluri <hdrive1999@gmail.com>
2018-09-07 23:23:27 +0530

    models added

--- application/models.py
+++ application/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+
+class Row(models.Model):
+    level = models.IntegerField(null=False, blank=False) # counting from the bottom
+    entry = models.ForeignKey("Entry", on_delete=models.CASCADE, null=False)
+    people = models.TextField(null=False, blank=False) # comma seperated, left-to-right from viewer's perspective
--- application/urls.py
+++ application/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import *
+
 
 app_name="applications"
 urlpatterns = [
-
+    #path('', views.index, name="index"),
 ]
