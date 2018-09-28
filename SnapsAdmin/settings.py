184c4ba5c2aaa36f4102ecbdb574c23765e1fa2e
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-29 04:33:38 +0530

    Oasis snap portal done

--- SnapsAdmin/settings.py
+++ SnapsAdmin/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'SnapsAdmin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'oasisdb.sqlite3'),
     }
 }
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/bosm-static/'
+STATIC_URL = '/oasis-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-LOGIN_URL = '/bosm-snaps/admin/'
+LOGIN_URL = '/oasis-snaps/admin/'
